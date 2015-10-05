4c538f1c28ce315af9f100e32eaf43d65b6bd66c
jlocas <jeremie.locas@hotmail.com>
2015-10-05 07:45:51 -0400

    changed collisions to use triggers instead added pyo project

---
+++ Pyo - Copie/OSC_02.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from pyo import *
+from SpringGridBlock import *
+
+s = Server(sr=44100, nchnls=2, buffersize=512, duplex=1).boot()
+
+blocks = [[]] #2d list of SpringGridBlock object
+gridLength = 0 #int
+gridSize = 0
+
+freqs = []
+amps = []
+
+instr = SumOsc(freq = 100, ratio=0.2, index=0.2, mul=0.1)
+
+
+def OSC(adress, *args):
+    
+    global gridLength
+    global gridSize
+    global blocks
+
+    if adress == '/springGrid/length':
+       gridLength = args[0]
+       gridSize = gridLength*gridLength
+       blocks = [[SpringGridBlock() for x in range(gridLength)] for z in range(gridLength)]
+       SetupSynth()
+
+    if adress == '/springGrid/blocks/positions':
+        i = 0
+        for x in range(gridLength):
+            for z in range(gridLength):
+                blocks[x][z].position.y = args[i]
+                i+=1
+                
+        UpdateSynth()
+        
+receiver = OscDataReceive(12543, "/springGrid/*", OSC)
+
+
+def SetupSynth():
+    global freqs
+    global instr
+    global amps
+    
+    fundFreq = 50
+    freqs = [0 for i in range(gridSize)]
+    amps = [0 for i in range(gridSize)]
+    i=0
+    
+    for x in range(gridLength):
+        for z in range(gridLength):
+            freqs[i] = fundFreq + fundFreq * (x + z)
+            i += 1
+            
+    instr = SumOsc(freq = freqs, ratio=1, index=0.5, mul=amps).out()
+    
+def UpdateSynth():
+    global amps
+    global freqs
+    
+    i = 0
+    
+    for x in range(gridLength):
+        for z in range(gridLength):
+            freqs[i] = freqs[i] + blocks[x][z].position.y
+            i+=1
+    
+    instr.setMul(0.01)
+    instr.setFreq(freqs)
+    
+instr.out()
+
+
+
+s.gui(locals())
---
+++ Unity/Assets/Scripts/OSCSender.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OSCSender : MonoBehaviour {
+
+	public SpringGrid grid;
+	private int gridLength;
+
+	Dictionary<string, ServerLog> servers = new Dictionary<string, ServerLog>();
+
+
+	// Use this for initialization
+	void Awake () {
+		gridLength = grid.GetSideLength();
+		OSCHandler.Instance.Init();
+
+		SendGridLength();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SendBlockPositions();
+	}
+
+	private void SendBlockPositions()
+	{
+		List<float> positions = new List<float>();
+
+		for(int x = 0; x < gridLength; x++){
+			for(int z = 0; z < gridLength; z++){
+
+				positions.Add(grid.GetBlock(x, z).Block.transform.position.x);
+				positions.Add(grid.GetBlock(x, z).Block.transform.position.y);
+				positions.Add(grid.GetBlock(x, z).Block.transform.position.z);
+
+			}
+		}
+		OSCHandler.Instance.SendMessageToClient("Pyo", "/springGrid/blocks/positions", positions);
+	}
+
+	private void SendGridLength()
+	{
+		OSCHandler.Instance.SendMessageToClient("Pyo", "/springGrid/length", gridLength);
+	}
+}
